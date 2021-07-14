ee03626f4ac0083e5daae6074eb1687cb7dffd4f
TarasSymovin <taras.simovin97@gmail.com>
2021-07-14 22:17:51 +0300

    add Spring_1 Intro & Core task

--- spring-1-intro-core-homework/src/main/java/ApplicationRunner.java
+++ spring-1-intro-core-homework/src/main/java/ApplicationRunner.java
@@ -1,6 +1,5 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ua.epam.symovin.collection.PrimaryTest;
 import ua.epam.symovin.configs.AppConfig;
 
 import java.util.Arrays;
--- spring-1-intro-core-homework/src/main/java/ua/epam/symovin/configs/AppConfigFirst.java
+++ spring-1-intro-core-homework/src/main/java/ua/epam/symovin/configs/AppConfigFirst.java
@@ -2,10 +2,8 @@ package ua.epam.symovin.configs;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = "ua.epam.symovin.beans1")
-@Import(AppConfigSecond.class)
 public class AppConfigFirst {
 }
